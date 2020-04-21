4eb8f39bdae72bac4965e240e97002d9c9aaacb0
smudge <nathan@ngriffith.com>
2020-04-21 00:45:56 -0400

    Add 'temp' command for configuring color temperature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate objc;
 
 use objc::rc::StrongPtr;
-use objc::runtime::{Object, BOOL};
+use objc::runtime::{Object, BOOL, YES};
 use objc::{class, msg_send, sel, sel_impl};
 use std::env::args;
 use std::process::exit;
@@ -18,8 +18,9 @@ fn print_usage(program: &String) {
     println!("{}\n", env!("CARGO_PKG_DESCRIPTION"));
     println!("Usage:\n  {} [command]\n", program);
     println!("Available Commands:");
-    println!("  on         Turns Night Shift on (until tomorrow/sunrise)");
-    println!("  off        Turns Night Shift off");
+    println!("  on                  Turns Night Shift on (until tomorrow/sunrise)");
+    println!("  off                 Turns Night Shift off");
+    println!("  temp [0-100]        Set color temperature to value");
 }
 
 fn main() {
@@ -31,10 +32,12 @@ fn main() {
     }
 
     let night_shift = NightShift::new();
-    if args[1] == "on" {
+    if args.len() == 2 && args[1] == "on" {
         night_shift.enable(true);
-    } else if args[1] == "off" {
+    } else if args.len() == 2 && args[1] == "off" {
         night_shift.enable(false);
+    } else if args.len() == 3 && args[1] == "temp" {
+        night_shift.set_temp(&args[2]);
     } else {
         print_usage(&args[0]);
     }
@@ -60,6 +63,21 @@ impl NightShift {
         let result: BOOL = unsafe { msg_send![*self.client, setEnabled: enabled] };
         if result != (true as BOOL) {
             eprintln!("Failed to toggle Night Shift!");
+            exit(1);
+        }
+    }
+
+    pub fn set_temp(&self, temp: &String) {
+        let temp = match temp.parse::<f32>() {
+            Ok(v) => v / 100.0,
+            Err(_) => {
+                eprintln!("Invalid temperature value! Please choose a number between 0 and 100.");
+                exit(1);
+            }
+        };
+        let result: BOOL = unsafe { msg_send![*self.client, setStrength:temp commit:YES] };
+        if result != (true as BOOL) {
+            eprintln!("Failed to set color balance!");
             exit(1);
         }
     }
