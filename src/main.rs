453514518df316d8b7312288a5de6e89cb987595
smudge <nathan@ngriffith.com>
2020-04-20 22:32:59 -0400

    It's alive! It turns Night Shift on!

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,26 @@
-fn main() {}
+extern crate objc;
+
+use objc::rc::StrongPtr;
+use objc::runtime::{Object, BOOL};
+use objc::{class, msg_send, sel, sel_impl};
+
+#[link(name = "CoreBrightness", kind = "framework")]
+extern "C" {}
+
+fn main() {
+    let client_class = class!(CBBlueLightClient);
+    let client = unsafe {
+        let obj: *mut Object = msg_send![client_class, alloc];
+        let obj: *mut Object = msg_send![obj, init];
+        StrongPtr::new(obj)
+    };
+
+    let enabled = true as BOOL;
+    let result: BOOL = unsafe { msg_send![*client, setEnabled: enabled] };
+    if result != enabled {
+        eprintln!(
+            "WARNING: Attempted to set enabled to {}, but received {}",
+            enabled, result
+        );
+    }
+}
