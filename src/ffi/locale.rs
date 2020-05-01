35da28d276d51baaec796cad0f5b3d0cba72247a
smudge <nathan@ngriffith.com>
2020-04-30 23:34:51 -0400

    Handle error case more gracefully here

--- src/ffi/locale.rs
+++ src/ffi/locale.rs
@@ -40,13 +40,12 @@ impl Locale {
         // Get the system to tell what time format is used for this locale:
         let format: *const NSString =
             unsafe { msg_send![nsdateformatter, dateFormatFromTemplate:j options:0 locale:locale] };
-        let format = match unsafe { format.as_ref() } {
-            Some(s) => s.as_str(),
-            None => "ERROR",
-        };
 
-        Ok(Locale {
-            is_24_hr: !format.contains("a"),
-        })
+        match unsafe { format.as_ref() } {
+            Some(f) => Ok(Locale {
+                is_24_hr: !f.as_str().contains("a"),
+            }),
+            None => Err("Unable to determine system locale!".to_string()),
+        }
     }
 }
